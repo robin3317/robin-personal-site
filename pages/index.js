1d99f20bf31a67201fa651379ee782d8a69a3d06
robin3317 <abdurrahmanrobin47@gmail.com>
2019-04-29 00:14:20 +0600

    Basic navigation setup

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import React, { Component, Fragment } from 'react';
 
+import Header from '../components/shared/Header';
+
 class Index extends Component {
   render() {
     return(
       <Fragment>
+        <Header />
         <h1>Index Page</h1>
       </Fragment>
     );
