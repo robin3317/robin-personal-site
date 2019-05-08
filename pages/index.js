ca1f7e1788363493b11b3865ce67f6d00605e569
robin3317 <abdurrahmanrobin47@gmail.com>
2019-05-09 00:14:37 +0600

    Getting data from fake api

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,29 @@ import axios from 'axios'
 import BaseLayout from '../components/layouts/BaseLayout'
 
 class Index extends Component {
+  static async getInitialProps() {
+    let userData = {}
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+      userData = response.data
+    }catch(error) {
+      console.error(error)
+    }
+
+    return {initialData: [1, 2, 3], userData}
+  }
+
+  state = {
+    title: 'I am index page'
+  }
+
   render() {
+    const {userData} = this.props;
+
     return(
       <BaseLayout>
         <h1>Index Page</h1>
+        <h2>{userData.title}</h2>
       </BaseLayout>
     );
   }
