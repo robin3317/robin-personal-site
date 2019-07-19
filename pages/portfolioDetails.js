6b54cbc95c948cbd94c34739f3ff66ade8ee0f2f
robin3317 <abdurrahmanrobin47@gmail.com>
2019-07-20 01:06:29 +0600

    dynamic routing

--- pages/portfolioDetails.js
+++ pages/portfolioDetails.js
@@ -2,15 +2,33 @@ import React, { Component } from 'react'
 import { withRouter } from 'next/router'
 
 import BaseLayout from '../components/layouts/BaseLayout'
+import axios from 'axios';
 
 class PortfolioDetails extends Component {
+
+  static async getInitialProps({query}) {
+    let postObj = {},
+        postId = query.id
+
+    try {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      postObj = response.data
+    }catch (error) {
+      console.error(error)
+    }
+
+    return postObj;
+  }
+
   render() {
-    const { id } = this.props.router.query
+    const {title, id, body} = this.props
 
     return(
       <BaseLayout>
         <h1>PortfolioDetails Page</h1>
-        <h2>{id}</h2>
+        <h2>{title}</h2>
+        <p>ID: {id}</p>
+        <p>BODY: {body}</p>
       </BaseLayout>
     )
   }
